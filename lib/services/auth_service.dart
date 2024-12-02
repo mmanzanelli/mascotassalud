d2004aa04fe9052488f3e6c5b711ab54355031ae
mmanzanelli <maximancast@gmail.com>
2024-12-02 20:43:02 +0100

    comprobaciones con ip de portatil

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,22 +2,22 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  static const String _baseUrl = 'http://localhost:3000/api';
+  static const String _baseUrl = 'http://192.168.11.176:3000';
 
   static Future<bool> login(String email, String password) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/usuarios/login'),
+      Uri.parse('$_baseUrl/login'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'correo': email, 'contraseña': password}),
+      body: jsonEncode({'email': email, 'password': password}),
     );
     return response.statusCode == 200;
   }
 
   static Future<bool> register(String email, String password) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl/usuarios/register'),
+      Uri.parse('$_baseUrl/register'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'correo': email, 'contraseña': password}),
+      body: jsonEncode({'email': email, 'password': password}),
     );
     return response.statusCode == 200;
   }
